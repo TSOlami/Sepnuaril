4c620dc7152db25d43d5e6e79517f672c7f1ca7a
freemann firms <freemannfirm@gmail.com>
2025-05-29 23:08:38 +0100

    Enhance styling and responsiveness across components; update color variables for dark mode support

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -95,11 +95,11 @@ const Homepage = () => {
           <div className="container relative z-10 flex flex-col-reverse lg:flex-row-reverse items-center justify-between p-2 md:p-6 mx-auto lg:py-16">
             {/* TEXT */}
             <div className="w-full lg:w-1/2 px-2 md:px-4">
-              <div className="text-center lg:text-left bg-none lg:bg-[var(--secondary-color)] dark:bg-gray-900/70 p-4 rounded">
-                <h1 className="text-3xl font-semibold tracking-wide text-[var(--tertiary-color)] dark:text-white lg:text-4xl pb-3">
+              <div className="text-center lg:text-left bg-none lg:bg-[var(--secondary-color)] p-4 rounded">
+                <h1 className="text-3xl font-semibold tracking-wide text-[var(--tertiary-color)] lg:text-4xl pb-3">
                   ABOUT US
                 </h1>
-                <p className="text-base text-[var(--tertiary-color)] dark:text-gray-300 text-justify">
+                <p className="text-base text-[var(--tertiary-color)] dark:text-gray-100 text-justify">
                   At Sepnuaril, we are dedicated to powering industries with
                   reliable oil and gas solutions. With a strong commitment to
                   efficiency, innovation, and sustainability, we specialize in
@@ -188,140 +188,56 @@ const Homepage = () => {
               </div>
             </div>
           </div>
-          <div className="container mb-6">
-            <div className="card bg-[var(--primary-color)] text-tertiary w-72 md:w-48 lg:w-72 shadow-xl mx-auto">
-              <figure>
-                <img
-                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                  alt="Shoes"
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">Card Title</h2>
-                <p>
-                  A card component has a figure, a body part, and inside body
-                  there are title and actions parts
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="container mb-6">
-            <div className="card bg-[var(--primary-color)] text-tertiary w-72 md:w-48 lg:w-72 shadow-xl mx-auto">
-              <figure>
-                <img
-                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                  alt="Shoes"
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">Card Title</h2>
-                <p>
-                  A card component has a figure, a body part, and inside body
-                  there are title and actions parts
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="container mb-6">
-            <div className="card bg-[var(--primary-color)] text-tertiary w-72 md:w-48 lg:w-72 shadow-xl mx-auto">
-              <figure>
-                <img
-                  src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-                  alt="Shoes"
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">Card Title</h2>
-                <p>
-                  A card component has a figure, a body part, and inside body
-                  there are title and actions parts
-                </p>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
 
       <div>
-        <section className="dark:bg-gray-900">
-          <div className="flex flex-col lg:flex-row items-center justify-between p-2 md:p-6 mx-auto lg:py-16">
-            {/* TEXT */}
-            <div className="w-full px-2 md:px-4">
-              <div className="text-center shadow-lg lg:text-left bg-[var(--primary-color)] dark:bg-gray-900/70 p-4 rounded">
-                <h1 className="text-md font-semibold tracking-wide text-[var(--tertiary-color)] dark:text-white lg:text-2xl pb-3">
-                  Interested in a career?
-                </h1>
-                <p className="text-sm text-[var(--tertiary-color)] dark:text-gray-300 text-justify p-4">
-                  At Sepnuaril, we believe that our people are our greatest
-                  asset. We're always on the lookout for talented, passionate,
-                  and forward-thinking individuals to join our growing team.
-                  Explore our current career opportunities to find a role that
-                  matches your skills and ambitions. From engineering and
-                  operations to business development and support functions, we
-                  offer diverse paths to help you grow professionally. Submit
-                  your application and upload your resume through our official
-                  job portal.
-                  <span className="text-red-500 text-sm font-bold">
-                    📌 Please note: Applications must be submitted via the
-                    official portal to be considered for employment.
-                  </span>
-                </p>
-                <p className="text-sm text-[var(--tertiary-color)] dark:text-gray-300 text-justify p-4">
-                  Need Help During the Application Process? We understand that
-                  applying for a new job can be a big step. If you have
-                  questions:
-                </p>
-                <ul className="dark:text-gray-300 space-y-4 text-tertiary text-sm">
-                  <li>
-                    ✅ <strong>Schedule a one-on-one call</strong> with our
-                    recruitment team — we’ll reach out at your preferred time to
-                    guide you through the process.
-                  </li>
-                  <li>
-                    🔍 <strong>Learn more</strong> about what it's like to work
-                    at Sepnuaril, our values, and the impact you can make in the
-                    energy sector.
-                  </li>
-                  <li className="border-l-4 border-yellow-400 pl-4">
-                    ⚠️{" "}
-                    <strong className="text-yellow-600 dark:text-yellow-400">
-                      Beware of fraudulent offers.
-                    </strong>
-                    <br />
-                    Sepnuaril does not request payment for job applications or
-                    training. All communication regarding recruitment will come
-                    from verified company sources.
-                  </li>
-                </ul>
-                <h1 className="text-md font-semibold tracking-wide text-[var(--tertiary-color)] dark:text-white lg:text-xl pb-2 pt-4">
-                  Trade Partner or Vendor?{" "}
-                </h1>
-                <p className="text-sm text-[var(--tertiary-color)] dark:text-gray-300 text-justify pb-4">
-                  Let’s Work Together We’re committed to building strong,
-                  transparent, and mutually beneficial relationships with trade
-                  partners. If you're a contractor, supplier, or service
-                  provider looking to work with Sepnuaril, please complete our
-                  Trade Partner Prequalification process. This ensures
-                  compliance, alignment with our values, and a shared commitment
-                  to safety and excellence. Other Questions or Requests? For all
-                  other inquiries, including collaborations, support, or general
-                  information: 📄 Please complete the contact form, and a member
-                  of our team will get back to you promptly.
-                </p>
-              </div>
+        <section className="">
+          <div className="w-full relative shadow-xl">
+            <div className="flex items-center justify-center mt-10 mx-4 md:mx-4">
+              <div className="border-t-2 border-[var(--primary-color)] w-48 md:w-96"></div>
+              <span className="px-6 md:px-14 text-secondary text-lg font-medium uppercase whitespace-nowrap">
+                CONTACT
+              </span>
+              <div className="border-t-2 border-[var(--primary-color)] w-48 md:w-96"></div>
             </div>
 
-            {/* FORM */}
-            <div className="w-full relative shadow-xl">
-              <section class="bg-white dark:bg-gray-900">
+            <section className="flex flex-col pt-6 md:pt-12 md:flex-row md:space-y-0 md:items-center md:justify-center">
+              <div className="flex md:flex-1 p-6 bg-[var(--primary-color)] shadow-md text-white font-medium h-[14rem] dark:bg-gray-100 dark:text-gray-900">
+                GENERAL ENQUIRY
+                <br />
+                <br />
+                info@sepnuaril.com
+              </div>
+              <div className="flex md:flex-1 p-6 bg-base-900 shadow-md h-[14rem]"></div>
+            </section>
+            <section className="flex flex-col-reverse md:flex-row md:space-y-0 md:items-center md:justify-center">
+              <div className="flex md:flex-1 p-6 bg-base-900 shadow-md h-[14rem]"></div>
+              <div className="flex md:flex-1 p-6 bg-[var(--primary-color)] shadow-md text-white font-medium h-[14rem] dark:bg-gray-100 dark:text-gray-900" >
+                COOPERATE OFFICE  - HQ
+                <br />
+                <br />
+                Address..................................................................
+                <br />
+                Number........................................................................
+              </div>
+            </section>
+            <section className="flex flex-col md:flex-row md:space-y-0 md:items-center md:justify-center">
+              <div className="flex md:flex-1 p-6 bg-[var(--primary-color)] shadow-md text-white font-medium h-[14rem] dark:bg-gray-100 dark:text-gray-900">
+                BRANCH OFFICE
+                <br />
+                <br />
+                Address..................................................................
+                <br />
+                Number.........................................................................
+              </div>
+              <div className="flex md:flex-1 p-6 bg-base-900 shadow-md h-[14rem]"></div>
+            </section>
+            {/* <section class="bg-white dark:bg-gray-900">
                 <div class="py-8 lg:py-12 px-4 mx-auto">
                   <h2 class="mb-2 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
                     Contact Us
                   </h2>
-                  {/* <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-                    Got a technical issue? Want to send feedback about a beta
-                    feature? Need details about our Business plan? Let us know.
-                  </p> */}
                   <form action="#" class="space-y-6">
                     <div>
                       <label
@@ -390,8 +306,7 @@ const Homepage = () => {
                     </button>
                   </form>
                 </div>
-              </section>
-            </div>
+              </section> */}
           </div>
         </section>
       </div>
